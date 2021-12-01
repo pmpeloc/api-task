8240ebe632214cce75382c4ccb589446e1dd42a1
Pablo Misael Peloc <pmpeloc@gmail.com>
2021-12-01 17:16:50 -0300

    Controladores, Servicios, DTO, Interfaces y Módulo

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,19 +1,13 @@
 module.exports = {
   parser: '@typescript-eslint/parser',
-  parserOptions: {
-    project: 'tsconfig.json',
-    sourceType: 'module',
-  },
+  parserOptions: { project: 'tsconfig.json', sourceType: 'module' },
   plugins: ['@typescript-eslint/eslint-plugin'],
   extends: [
     'plugin:@typescript-eslint/recommended',
     'plugin:prettier/recommended',
   ],
   root: true,
-  env: {
-    node: true,
-    jest: true,
-  },
+  env: { node: true, jest: true },
   ignorePatterns: ['.eslintrc.js'],
   rules: {
     '@typescript-eslint/interface-name-prefix': 'off',
